5174ae1c846a4dcc17c0c0f1b2ad72c0d2341c9b
tdobson <hello@tdobson.net>
2024-06-25 10:50:40 +0100

    Refactor Header component to use Mantine Header and Container components.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import { Tabs } from '@mantine/core';
+import { Tabs, Header as MantineHeader, Container } from '@mantine/core';
 import { Link } from 'wouter';
-import './Header.css'; // Import the CSS file for Header styling
 
 function Header({ location, handleTabChange }) {
     console.log('Header component rendered with props:', { location, handleTabChange });
     return (
-        <header className="header">
-            <Tabs value={location} onChange={handleTabChange}>
-                <Tabs.List>
-                    <Tabs.Tab value="/" component={Link} to="/">neoClan</Tabs.Tab>
-                    <Tabs.Tab value="/features" component={Link} to="/features">Features</Tabs.Tab>
-                    <Tabs.Tab value="/download" component={Link} to="/download">Download</Tabs.Tab>
-                    <Tabs.Tab value="/contribute" component={Link} to="/contribute">Contribute</Tabs.Tab>
-                </Tabs.List>
-            </Tabs>
-        </header>
+        <MantineHeader height={60} p="xs">
+            <Container>
+                <Tabs value={location} onTabChange={handleTabChange}>
+                    <Tabs.List>
+                        <Tabs.Tab value="/" component={Link} to="/">neoClan</Tabs.Tab>
+                        <Tabs.Tab value="/features" component={Link} to="/features">Features</Tabs.Tab>
+                        <Tabs.Tab value="/download" component={Link} to="/download">Download</Tabs.Tab>
+                        <Tabs.Tab value="/contribute" component={Link} to="/contribute">Contribute</Tabs.Tab>
+                    </Tabs.List>
+                </Tabs>
+            </Container>
+        </MantineHeader>
     );
 }
 
